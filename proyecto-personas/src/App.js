5e1291411a090985df9cde18ea30989f45145298
DiegoBtts <die-leo@outlook.com>
2020-04-24 01:10:27 -0700

    Modificacion de App.js para que se agregara los detalles de los usuarios

--- proyecto-personas/src/App.js
+++ proyecto-personas/src/App.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import './App.css';
 
 import Home from './views/Home'
+import UserDetail from './views/UserDetail'
 
 class App extends Component {
   render() {
@@ -11,10 +12,15 @@ class App extends Component {
       <Router history={ this.props.history }>
         <div className="App">
           <Route exact path="/" component={ Home }/>
+          <Route path="/detail/:userId" component={ UserDetail }/> 
         </div>
       </Router>
     );
   }
+}
+
+App.propTypes = {
+  history: PropTypes.any.isRequired
 }
 
 export default App;
